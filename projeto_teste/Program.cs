b8aaa6abc63544a6f63edddeda0e239084e33833
William Fernando <wfernandofrancisco@hotmail.com>
2019-10-24 10:16:18 -0300

    Projeto iniciado, mostrando a mensagem ola mundo

---
+++ projeto_teste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
